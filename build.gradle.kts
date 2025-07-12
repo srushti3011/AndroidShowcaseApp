10954f855a36738bec468ecf1deab0ea5ebd9be6
srushti engineer <srushtie123@gmail.com>
2025-07-12 16:50:18 +0530

    ShowcaseApp-AppLogo-Dependencies: App Logo Setup and Dependencies for Login, Signup, Onboarding

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
+buildscript {
+    repositories {
+        google()
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
